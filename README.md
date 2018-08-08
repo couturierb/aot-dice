782b978b2cb8ff1cbd9e203853b9a9236e7a1d9d
Myzuno <myzuno@MacBook-Pro-de-Myzuno.local>
2018-08-08 09:01:21 +0200

    adapter l'ecran aux mobiles (En cours)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # aot-dice
 playground for css transform and progressive webapp
+
+# todo
+- Adapter aux petits ecrans
+- Travailler et ajouter les images des faces du des
+- Faire les icones pour l'app
+- Ajouter l'effet 'bounce'
--- main.js
+++ main.js
@@ -1,42 +1,29 @@
+const animation = [
+    {name : 'spinToFront', occurence : 0},
+    {name : 'spinToBack', occurence : 0},
+    {name : 'spinToRight', occurence : 0},
+    {name : 'spinToLeft', occurence : 0},
+    {name : 'spinToTop', occurence : 0},
+    {name : 'spinToBottom', occurence : 0}
+];
+
 function roll() {
+    let rand = Math.floor(Math.random() * Math.floor(6));
     let element = document.getElementById("cube");
     let newElem = element.cloneNode(true);
-    newElem.style.animation = `${getAnimationName()} 2s cubic-bezier(0.19, 1, 0.22, 1) forwards`;
-    // newElem.style["animation-name"] = getAnimationName();
-    // newElem.style["animation-duration"] = "2s";
-    // newElem.style["animation-timing-function"] = "linear";
-    // newElem.style["animation-fill-mode"] = "forwards";
-    // newElem.classList.add("bounce2");
+
+    animation[rand].occurence++;
+    newElem.style.animation = `${animation[rand].name} 2s cubic-bezier(0.19, 1, 0.22, 1) forwards`;
     element.parentNode.replaceChild(newElem, element);
 }
 
-const getAnimationName = () => {
-    switch (random()) {
-        case 0 :
-            document.getElementById("resume-front").innerText++;
-            return "spinToFront";
-            break;
-        case 1 :
-            document.getElementById("resume-back").innerText++;
-            return "spinToBack"
-            break;
-        case 2 :
-            document.getElementById("resume-right").innerText++;
-            return "spinToRight"
-            break;
-        case 3 :
-            document.getElementById("resume-left").innerText++;
-            return "spinToLeft"
-            break;
-        case 4 :
-            document.getElementById("resume-top").innerText++;
-            return "spinToTop"
-            break;
-        case 5 :
-            document.getElementById("resume-bottom").innerText++;
-            return "spinToBottom"
-            break;
-    }
-}
 
-const random = () => Math.floor(Math.random() * Math.floor(6));
+
+
+(function() {
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker
+                 .register('./sw.js')
+                 .then(function() { console.log("Service Worker Registered"); });
+    }
+})();
